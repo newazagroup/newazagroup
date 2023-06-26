026ab56a7f26172edc23f355aeaa70fb505674dc
newazagroup <fomanyif@gmail.com>
2023-06-25 19:10:00 -0500

    subnet added

--- main.tf
+++ main.tf
@@ -12,3 +12,13 @@ resource "aws_vpc" "dev-vpc" {
   }
 }
 
+resource "aws_subnet" "dev-public-subnet" {
+    vpc_id = aws_vpc.dev-vpc.id
+    cidr_block = "10.0.0.0/24"
+    availability_zone = "us-east-1a"
+    map_public_ip_on_launch = true
+
+    tags = {
+      "Name" = "dev-public-subnet"
+    }
+}
